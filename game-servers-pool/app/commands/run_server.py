7f291b41ac48738dbadf8ca036764032046cdb4e
Valeryi Savich <relrin78@gmail.com>
2018-05-26 14:56:17 +0300

    Added running microservice register worker before starting app

--- game-servers-pool/app/commands/run_server.py
+++ game-servers-pool/app/commands/run_server.py
@@ -1,6 +1,8 @@
+import asyncio
 from sanic_script import Command, Option
 
 from app import app
+from app.workers.microservice_register import MicroserviceRegisterWorker
 
 
 class RunServerCommand(Command):
@@ -14,7 +16,15 @@ class RunServerCommand(Command):
         Option('--port', '-p', dest='port'),
     )
 
+    def register_microservice(self):
+        loop = asyncio.get_event_loop()
+        worker = MicroserviceRegisterWorker(self.app)
+        loop.run_until_complete(worker.run(loop=loop))
+        loop.stop()
+        loop.close()
+
     def run(self, *args, **kwargs):
+        self.register_microservice()
         self.app.run(
             host=kwargs.get('host', None) or self.app.config["APP_HOST"],
             port=kwargs.get('port', None) or self.app.config["APP_PORT"],
